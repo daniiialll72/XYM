ec4777200f4c11645a50a8f865690ed4ec70e85f
danial <dania_khaledi@yahoo.com>
2020-10-28 12:30:51 +0330

    aaaaaaaaaaaa

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,7 +18,7 @@
      <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
      <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
     </div>
-   </div>
+   </div> 
 
    <div class="max-w-sm rounded overflow-hidden shadow-lg">
     <img class="w-full" src="/img/card-top.jpg" alt="Sunset in the mountains">
